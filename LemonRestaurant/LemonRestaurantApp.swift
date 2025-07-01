18458420b1a798aadc9cee72137169547771d406
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2025-06-30 19:58:00 -0600

    Initial Commit

---
+++ LemonRestaurant/LemonRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  LemonRestaurantApp.swift
+//  LemonRestaurant
+//
+//  Created by Samantha Jimenez on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct LemonRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
