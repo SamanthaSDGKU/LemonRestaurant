f8f6dca34a92a22da239053bc077060fc5449ad6
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2025-07-02 18:21:26 -0600

    creating AboutView

---
+++ LemonRestaurant/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  LemonRestaurant
+//
+//  Created by Samantha Jimenez on 02/07/25.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
